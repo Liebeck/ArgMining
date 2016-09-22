4534b55bbad15ebc77f90ab38424a8ba9355bbed
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-09-22 18:34:29 +0200

    Lowercase project name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='ArgMining',
+    name='argmining',
     version='0.0.1',
     description='Machine learning environment for paper *What to do with an airport? Mining Arguments in the German Online Participation Project Tempelhofer Feld*',
     url='http://dbs.cs.uni-duesseldorf.de',
