f4e30fb6b4090d11f73df930cce7ea4c6cc53db1
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-09-22 17:29:25 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='ArgMining',
+    version='0.0.1',
+    description='Machine learning environment for paper *What to do with an airport? Mining Arguments in the German Online Participation Project Tempelhofer Feld*',
+    url='http://dbs.cs.uni-duesseldorf.de',
+    author='HHU Duesseldorf',
+    author_email='liebeck@cs.uni-duesseldorf.de',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=[
+        'pandas_confusion'
+    ]
+)
