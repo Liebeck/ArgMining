7b87c76fb53a535477973bff44dbef8fd2bbf937
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-04 17:53:27 +0100

    Use JSON instead of XML

--- argmining/sentence/loaders/THF_sentence_corpus_loader.py
+++ argmining/sentence/loaders/THF_sentence_corpus_loader.py
@@ -2,62 +2,45 @@ from argmining.models.thf_sentence_export import THFSentenceExport
 from argmining.models.token import Token
 import logging
 import xml.etree.ElementTree as ET
+import json
 
 logger = logging.getLogger()
 
 
-def parse_tree_tagger_lemma(node):
+def parse_IWNLP_lemma(text):
     return None
 
 
-def load(file_path='data/THF/sentence/subtask_A_training.xml'):
+def parse_tree_tagger_lemma(text):
+    return None
+
+
+def load(file_path='data/THF/sentence/subtaskA_train.json'):
     """
-    Loads the THF corpus from an XML filegit
-    :param file_path: relative path to the XML file
+    Loads the THF corpus from an JSON file
+    :param file_path: relative path to the JSON file
     :return:
     """
-    logger.debug(u'Parsing XML File: {}'.format(file_path))
+    logger.debug(u'Parsing JSON File: {}'.format(file_path))
+
     sentences = []
-    root = ET.parse(file_path).getroot()
+    with open(file_path, encoding='utf-8') as data_file:
+        data = json.load(data_file)
+        for sentence in data:
+            sentence_tokens = sentence["NLP"]["Sentences"][0]["Tokens"]
+            tokens = []
+            for token in sentence_tokens:
+                token_model = Token(token["TokenIndexInSentence"],
+                                    token["Text"],
+                                    token["POSTag"],
+                                    token["MateToolsPPOS"],
+                                    token["MateToolsPLemma"],
+                                    parse_tree_tagger_lemma(token["TreeTaggerLemma"]),
+                                    parse_IWNLP_lemma(token["IWNLPLemma"]),
+                                    token.get("Polarity", None))
+                tokens.append(token_model)
+            sentence_model = THFSentenceExport(sentence["UniqueID"], sentence["Label"], sentence["Text"], tokens)
 
-    for sentence in root:
-        print(sentence.tag)
-        unique_ID = sentence.find("UniqueID").text
-        label = sentence.find("Label").text
-        text = sentence.find("Text").text
-        xml_tokens = sentence.find("NLP").find("Sentences").find("Sentence").find("Tokens")
-        xml_dependencies = sentence.find("NLP").find("Sentences").find("Sentence").find("Dependencies")
-        tokens = []
-        for xml_token in xml_tokens.getchildren():
-            tree_tagger_lemma_node = xml_token.find("TreeTaggerLemma").text
-            tree_tagger_lemma = None
-            iwnlp_lemma_node = xml_token.find("IWNLPLemma").text
-            iwnlp_lemma = None
-            polarity_node = xml_token.find("Polarity").text
-            polarity = None
-            token_model = Token(xml_token.find("TokenIndexInSentence").text,
-                                xml_token.find("Text").text,
-                                xml_token.find("POSTag").text,
-                                xml_token.find("MateToolsPPOS").text,
-                                xml_token.find("MateToolsPLemma").text,
-                                tree_tagger_lemma,
-                                iwnlp_lemma,
-                                polarity)
-        # print(item.tag)
-        #    def __init__(self, token_index_in_sentence, text, pos_tag, mate_tools_pos_tag, mate_tools_lemma, tree_tagger_lemma,iwnlp_lemma, polarity):
-
-        # print(nlp)
-        # children = sentence.getchildren()
-        # print(sentence.find("UniqueID").text)
-        # print(sentence.getchildren().find("entry")['UniqueID'])
-        # print(ET.tostring(ET.SubElement(sentence, 'UniqueID')))
-        # print(ET.SubElement(sentence, 'UniqueID').attrib)
-        # print(ET.SubElement(sentence, 'UniqueID').text)
-        # with open(absolute_path) as fp:
-        # xml = '\n'.join(fp.readlines())
-        # soup = BeautifulSoup(xml, 'xml')
-        # print(soup)
-        sentence_model = THFSentenceExport(unique_ID, label, text, tokens)
-        sentences.append(sentence_model)
-
-    logger.info('Parsed {} sentences'.format(len(sentences)))
+            sentences.append(sentence_model)
+            # print(json.dumps(token, indent=4, sort_keys=True))
+            logger.info('Parsed {} sentences'.format(len(sentences)))
