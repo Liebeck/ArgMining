a5a222b72000f1f12c746fee28050fe950c2a1e7
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-06 17:41:13 +0100

    Add tests

--- tests/loaders/test_THF_sentence_corpus_loader.py
+++ tests/loaders/test_THF_sentence_corpus_loader.py
@@ -1,6 +1,7 @@
 import unittest
 from argmining.sentence.loaders.THF_sentence_corpus_loader import parse_tree_tagger_lemma
 from argmining.sentence.loaders.THF_sentence_corpus_loader import parse_IWNLP_lemma
+from argmining.sentence.loaders.THF_sentence_corpus_loader import parse_polarity
 
 
 class THFSentenceCorpusLoaderTests(unittest.TestCase):
@@ -12,7 +13,12 @@ class THFSentenceCorpusLoaderTests(unittest.TestCase):
     def test_tree_tagger_lemma_one_value(self):
         input_value = ["flächendeckend"]
         parsed_lemma = parse_tree_tagger_lemma(input_value)
-        self.assertEqual(parsed_lemma, "flächendeckend")
+        self.assertEqual(parsed_lemma, ["flächendeckend"])
+
+    def test_tree_tagger_lemma_two_values(self):
+        input_value = ["Rüge", "Rügen"]
+        parsed_lemma = parse_IWNLP_lemma(input_value)
+        self.assertEqual(parsed_lemma, ["Rüge", "Rügen"])
 
     def test_iwnlp_lemma_empty(self):
         input_value = []
@@ -22,7 +28,22 @@ class THFSentenceCorpusLoaderTests(unittest.TestCase):
     def test_iwnlp_lemma_one_value(self):
         input_value = ["Ferkel"]
         parsed_lemma = parse_IWNLP_lemma(input_value)
-        self.assertEqual(parsed_lemma, "Ferkel")
+        self.assertEqual(parsed_lemma, ["Ferkel"])
+
+    def test_iwnlp_lemma_two_values(self):
+        input_value = ["Rügen", "Rüge"]
+        parsed_lemma = parse_IWNLP_lemma(input_value)
+        self.assertEqual(parsed_lemma, ["Rügen", "Rüge"])
+
+    def test_polarity_empty(self):
+        input_value = None
+        parsed_polarity = parse_polarity(input_value)
+        self.assertEqual(parsed_polarity, None)
+
+    def test_polarity_with_value(self):
+        input_value = 0.004
+        parsed_polarity = parse_polarity(input_value)
+        self.assertEqual(parsed_polarity, 0.004)
 
 
 if __name__ == '__main__':
