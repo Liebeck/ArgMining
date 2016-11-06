059f65b238329c27dcf29dafaeaafe2b9f645fa4
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-06 18:16:12 +0100

    Add a model for dependencies

---
+++ argmining/models/dependency.py
@@ -0,0 +1,5 @@
+class Dependency:
+    def __init__(self, token_ID, releation, head_token_ID):
+        self.token_ID = token_ID
+        self.releation = releation
+        self.head_token_ID = head_token_ID
