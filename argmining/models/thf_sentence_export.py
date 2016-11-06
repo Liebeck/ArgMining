ca991ee4afb1f59a3d16c8b6fcfedb301101ed63
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-06 18:18:22 +0100

    Expand the sentence model

--- argmining/models/thf_sentence_export.py
+++ argmining/models/thf_sentence_export.py
@@ -1,6 +1,7 @@
 class THFSentenceExport:
-    def __init__(self, uniqueID, label, text, tokens):
+    def __init__(self, uniqueID, label, text, tokens, dependencies):
         self.uniqueID = uniqueID
         self.label = label
         self.text = text
         self.tokens = tokens
+        self.dependencies = dependencies
