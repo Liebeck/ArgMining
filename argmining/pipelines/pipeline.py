2e8022004c2c65f54829efb64693d01d86916fbf
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-07 18:04:16 +0100

    Remove print

--- argmining/pipelines/pipeline.py
+++ argmining/pipelines/pipeline.py
@@ -2,6 +2,5 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 
 
 def pipeline(strategy, classifier):
-    print(type(strategy))
     return Pipeline(steps=[('union', FeatureUnion(strategy)),
                            classifier])
