b7eb548e1afe57ea684d060356346cafb79653f3
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-07 15:54:34 +0100

    Fix pep8

--- test.py
+++ test.py
@@ -2,9 +2,7 @@
 # -*- coding: UTF-8 -*-
 from argmining.sentence.loaders.THF_sentence_corpus_loader import load
 
-
 taskA_train = load(file_path='data/THF/sentence/subtaskA_train.json')
 taskA_test = load(file_path='data/THF/sentence/subtaskA_test.json')
 taskB_train = load(file_path='data/THF/sentence/subtaskB_train.json')
 taskB_test = load(file_path='data/THF/sentence/subtaskB_test.json')
-
