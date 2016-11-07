7b855b3ca9f32775361e60a40a237205aae19578
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-07 17:55:02 +0100

    Rename module

--- argmining/stategies/strategies.py
+++
@@ -1,7 +0,0 @@
-import argmining.features.bag_of_words as bag_of_words
-
-FEATURES = {'unigram': [bag_of_words.build(ngram=1)],
-            'bigram': [bag_of_words.build(ngram=2)],
-            'unigram_bigram': [bag_of_words.build(ngram=1),
-                               bag_of_words.build(ngram=2)]
-            }
