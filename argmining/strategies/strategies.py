d7aecc13427b250ec2aeeb70c2306301ed06d63b
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-07 19:48:43 +0100

    Fix bug with FeatureUnion

--- argmining/strategies/strategies.py
+++ argmining/strategies/strategies.py
@@ -2,6 +2,6 @@ import argmining.features.bag_of_words as bag_of_words
 
 STRATEGIES = {'unigram': [bag_of_words.build(ngram=1)],
               'bigram': [bag_of_words.build(ngram=2)],
-              'unigram_bigram': [bag_of_words.build(ngram=1),
-                                 bag_of_words.build(ngram=2)]
+              'unigram_bigram': [bag_of_words.build(ngram=1, feature_name='unigram'),
+                                 bag_of_words.build(ngram=2, feature_name='bigram')]
               }
