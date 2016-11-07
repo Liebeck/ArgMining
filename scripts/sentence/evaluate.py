8b0c97cea0a732f7b093fd29d4cd51362cb8da7e
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-07 18:04:31 +0100

    Continue to work on the evaluate script

--- scripts/sentence/evaluate.py
+++ scripts/sentence/evaluate.py
@@ -1,6 +1,12 @@
 import argparse
 from argmining.sentence.loaders.THF_sentence_corpus_loader import load
 from time import time
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+from argmining.pipelines.pipeline import pipeline
+from argmining.strategies.strategies import STRATEGIES
+
+NJOBS = 1
 
 
 def config_argparser():
@@ -9,7 +15,7 @@ def config_argparser():
     argparser.add_argument('-subtask', type=str, required=True, help='Name of the subtask')
     argparser.add_argument('-strategy', type=str, required=True, help='Name of the strategy')
     argparser.add_argument('-c', '--classifier', type=str, required=True, help='Name of the classifier')
-    argparser.add_argument('--shuffle', type=int, help='k für Kreuzvalidierung', default=None)
+    argparser.add_argument('--shuffle', type=int, help='Random state of the shuffle or None', default=None)
     return argparser.parse_args()
 
 
@@ -17,12 +23,24 @@ if __name__ == '__main__':
     t0 = time()
     arguments = config_argparser()
     # 1) Load data sets
-    X_train = load(file_path='data/THF/sentence/subtask{}_train.json'.format(arguments.subtask))
-    y_test = load(file_path='data/THF/sentence/subtask{}_test.json'.format(arguments.subtask))
+    dataset = load(file_path='data/THF/sentence/subtask{}_train.json'.format(arguments.subtask))
 
+    X_train = dataset
+    y_train = [item.label for item in dataset]
+    # 2) Shuffle if desired
+    # 3)
+    param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+                  'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
+    classifier = SVC(kernel='rbf', class_weight='balanced', random_state=0)
+    # strategy = STRATEGIES[arguments.strategy]
 
-print("Total execution time in %0.3fs" % (time() - t0))
-print("*****************************************")
+    import argmining.features.bag_of_words as bag_of_words
 
-
-
+    strategy = [bag_of_words.build(ngram=1)]
+    # print(type(strategy))
+    # print(type(strategy[0][1]))
+    pipe = pipeline(strategy=strategy, classifier=classifier)
+    gridsearch = GridSearchCV(pipe, param_grid, scoring='f1_macro', cv=arguments.nfold, n_jobs=NJOBS)
+    gridsearch.fit(X_train, y_train)
+    print("Total execution time in %0.3fs" % (time() - t0))
+    print("*****************************************")
