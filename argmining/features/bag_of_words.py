e9c383ca3379a51426e8b94b594beebaada74a07
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-08 13:26:42 +0100

    Fix bag of words

--- argmining/features/bag_of_words.py
+++ argmining/features/bag_of_words.py
@@ -1,6 +1,7 @@
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
+import logging
 
 
 def build(ngram=1, feature_name='bag_of_words'):
@@ -11,32 +12,32 @@ def build(ngram=1, feature_name='bag_of_words'):
 
 
 def tokenizer_THF_words(thf_sentence):
-    print("tokenizer_THF_words called")
-    print(thf_sentence.tokens[0].text)
-    return list(map(lambda token: token.text, thf_sentence))
-
-
-def get_vocabulary(X):
-    print("get_vocabulary called")
-    vectorizer = CountVectorizer(tokenizer=tokenizer_THF_words)
-    vectorizer.fit(X)
-    return vectorizer.get_feature_names()
+    return list(map(lambda token: token.text, thf_sentence.tokens))
 
 
 class BagOfWords(BaseEstimator):
     def __init__(self, ngram=1, token_form='text'):
         self.ngram = ngram
         self.token_form = token_form
+        self.logger = logging.getLogger()
 
     def fit(self, X, y):
         if self.token_form == 'text':
-            self.vocabulary = get_vocabulary(X)
+            self.vectorizer = CountVectorizer(tokenizer=tokenizer_THF_words,
+                                              ngram_range=(self.ngram, self.ngram),
+                                              lowercase=False)  # the lowercase is workaround for passing a custom class
+            self.vectorizer.fit(X)
+            self.logger.info("Created a vocabulary with length {}".format(len(self.vectorizer.get_feature_names())))
             return self
         else:
             raise ValueError("token_form not implemented")
 
     def transform(self, X):
-        return list(map(lambda x: self.transform_sentence(x), X))
+        self.logger.debug("transform called")
+        transformed = list(map(lambda x: self.transform_sentence(x), X))
+        self.logger.debug("transform returning")
+        return transformed
 
-    def transform_sentence(self, x):
-        return list(map(lambda token: token.text, x.tokens))
+    def transform_sentence(self, thf_sentence):
+        vectorized = self.vectorizer.transform([thf_sentence])
+        return vectorized
