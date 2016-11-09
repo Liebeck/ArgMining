e66726acbab6ccd9ddbd42514670a88ed51d6872
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-09 16:08:43 +0100

    Add bigram iwnlp feature

--- argmining/strategies/strategies.py
+++ argmining/strategies/strategies.py
@@ -3,9 +3,10 @@ import argmining.features.bag_of_words as bag_of_words
 STRATEGIES = {'unigram': [bag_of_words.build(ngram=1)],
               'unigram_lowercase': [bag_of_words.build(ngram=1, lowercase=True)],
               'unigram_iwnlp': [bag_of_words.build(ngram=1, token_form='IWNLP_lemma')],
-              'unigram_iwnlp_lowercase': [bag_of_words.build(ngram=1, token_form='IWNLP_lemma',lowercase=True)],
+              'unigram_iwnlp_lowercase': [bag_of_words.build(ngram=1, token_form='IWNLP_lemma', lowercase=True)],
               'bigram': [bag_of_words.build(ngram=2)],
               'bigram_lowercase': [bag_of_words.build(ngram=2, lowercase=True)],
+              'bigram_iwnlp': [bag_of_words.build(ngram=2, token_form='IWNLP_lemma')],
               'unigram_bigram': [bag_of_words.build(ngram=1, feature_name='unigram'),
                                  bag_of_words.build(ngram=2, feature_name='bigram')]
               }
