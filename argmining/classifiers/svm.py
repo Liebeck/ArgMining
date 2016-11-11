680e01f3fa879a1dd388f8dbeb1a37689f80fa84
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-11 11:14:23 +0100

    Add old values for grid search

--- argmining/classifiers/svm.py
+++ argmining/classifiers/svm.py
@@ -5,6 +5,7 @@ from sklearn.svm import SVC
 def param_grid():
     return {'classifier__C': [1e3],
             'classifier__gamma': [0.005, 0.01]}
+    # return {'classifier__C': [1e3, 5e3, 1e4, 5e4, 1e5], 'classifier__gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
 
 
 def build(kernel='rbf', random_state=0):
