86589a3439c7195f6dd095fe51e245be5d8aaba0
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-11 18:57:32 +0100

    Add KNN classifier to the classifier selection

--- argmining/classifiers/classifier.py
+++ argmining/classifiers/classifier.py
@@ -1,8 +1,11 @@
 from argmining.classifiers import svm
+from argmining.classifiers import knn
 
 
 def get_classifier(name):
     if name == 'svm':
         return svm.build(), svm.param_grid()
+    elif name == 'knn':
+        return knn.build(), knn.param_grid()
     else:
         raise ValueError("Unknown classifier")
