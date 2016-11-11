4e3ca33a74e4438a56b2b8d9fa613a8462de1ea1
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-11 19:05:50 +0100

    Add random forest to the classifier selection

--- argmining/classifiers/classifier.py
+++ argmining/classifiers/classifier.py
@@ -1,5 +1,6 @@
 from argmining.classifiers import svm
 from argmining.classifiers import knn
+from argmining.classifiers import random_forest
 
 
 def get_classifier(name):
@@ -7,5 +8,7 @@ def get_classifier(name):
         return svm.build(), svm.param_grid()
     elif name == 'knn':
         return knn.build(), knn.param_grid()
+    elif name == 'rf':
+        return random_forest.build(), random_forest.param_grid()
     else:
         raise ValueError("Unknown classifier")
