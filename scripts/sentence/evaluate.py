5c7dec73c26c77700712537c3159425551fa7097
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-16 13:56:19 +0100

    Load datasets

--- scripts/sentence/evaluate.py
+++ scripts/sentence/evaluate.py
@@ -1,5 +1,5 @@
 import argparse
-from argmining.sentence.loaders.THF_sentence_corpus_loader import load
+from argmining.sentence.loaders.THF_sentence_corpus_loader import load_dataset
 import time
 from sklearn.model_selection import GridSearchCV
 import logging
@@ -27,8 +27,14 @@ if __name__ == '__main__':
     with open(arguments.configfile) as data_file:
         settings = json.load(data_file)
     # 2) Read datasets
-
-
+    X_train, y_train = load_dataset(file_path='data/THF/sentence/subtask{}_train.json'.format(settings['subtask']))
+    X_test, y_test = load_dataset(file_path='data/THF/sentence/subtask{}_test.json'.format(settings['subtask']))
+    # 3) Load classifier with arguments
+    # 4) Load features and set arguments
+    # 5) Train classifier
+    # 6) Predict the test set
+    # 7) Print the confusion matrix
+    # 8) Save the prediction into the file system
 
     logger.info("Total execution time in %0.3fs" % (time.time() - t0))
     logger.info("*****************************************")
