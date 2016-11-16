41d5c6f167bfe6d74e98cf12431e318bebceb470
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-16 19:42:41 +0100

    Change gridsearch.py to GRIDSEARCH_STRATEGIES

--- scripts/sentence/gridsearch.py
+++ scripts/sentence/gridsearch.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import GridSearchCV
 import logging
 import json
 from argmining.pipelines.pipeline import pipeline
-from argmining.strategies.strategies import STRATEGIES
+from argmining.strategies.gridsearch import GRIDSEARCH_STRATEGIES
 from argmining.evaluation.gridsearch_report import report_best_results, best_cv_result
 from argmining.classifiers.classifier import get_classifier
 from collections import OrderedDict
@@ -17,7 +17,7 @@ def config_argparser():
     argparser = argparse.ArgumentParser(description='ArgMining')
     argparser.add_argument('-nfold', type=int, help='n-fold crossvalidation', default=2)
     argparser.add_argument('-subtask', type=str, required=True, help='Name of the subtask')
-    argparser.add_argument('-strategy', type=str, required=True, help='Name of the strategy')
+    argparser.add_argument('-gridsearchstrategy', type=str, required=True, help='Name of the gridsearch strategy')
     argparser.add_argument('-c', '--classifier', type=str, required=True, help='Name of the classifier')
     argparser.add_argument('--shuffle', type=int, help='Random state of the shuffle or None', default=None)
     return argparser.parse_args()
@@ -30,15 +30,21 @@ if __name__ == '__main__':
     # 1) Load data sets
     X_train, y_train = load_dataset(file_path='data/THF/sentence/subtask{}_train.json'.format(arguments.subtask))
     # 2) Shuffle if desired
-    # 3) Select feature combination
-    logger.info("Using strategy: {}".format(arguments.strategy))
-    strategy = STRATEGIES[arguments.strategy]
 
-    # 4) Select classifier
+    # 3) Select classifier
     logger.info("Using classifier: {}".format(arguments.classifier))
     classifier, param_grid = get_classifier(arguments.classifier)
+    # 4) Select feature combination
+    logger.info("Using gridsearch strategy: {}".format(arguments.gridsearchstrategy))
+    strategy = GRIDSEARCH_STRATEGIES[arguments.gridsearchstrategy]['features']
+    param_grid.update(GRIDSEARCH_STRATEGIES[arguments.gridsearchstrategy]['param_grid'])
+    logger.info(param_grid)
+
+
+
     # 5) Start grid search
     pipe = pipeline(strategy=strategy, classifier=classifier)
+    logger.info(pipe)
     logger.info("Start grid search")
     gridsearch = GridSearchCV(pipe, param_grid, scoring='f1_macro', cv=arguments.nfold, n_jobs=NJOBS, verbose=2)
     gridsearch.fit(X_train, y_train)
@@ -47,7 +53,7 @@ if __name__ == '__main__':
     # 6) Serialize the best settings
     settings = OrderedDict()
     settings['classifier'] = arguments.classifier
-    settings['strategy'] = arguments.strategy
+    settings['gridsearchstrategy'] = arguments.gridsearchstrategy
     settings['subtask'] = arguments.subtask
     settings['nfold'] = arguments.nfold
     settings['shuffle'] = arguments.shuffle
