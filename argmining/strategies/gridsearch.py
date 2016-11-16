d916d52dd0ab9e6b33623f9d07bdab05e1c00dcd
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-16 19:06:56 +0100

    Add gridsearch strategies with parameters

---
+++ argmining/strategies/gridsearch.py
@@ -0,0 +1,12 @@
+import argmining.features.bag_of_words as bag_of_words
+import argmining.features.pos_distribution as pos_distribution
+import argmining.features.dependency_distribution as dependency_distribution
+import argmining.features.structural_features as structural_features
+
+GRIDSEARCH_STRATEGIES = {'bag_of_words':
+                             {'features': [bag_of_words],
+                              'param_grid': {
+                                  'transformer__bag_of_words': [1, 2]
+                              }
+                              }
+                         }
