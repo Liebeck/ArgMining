2fb193d1299ddab2919d9d74b3c372840d93d90c
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-18 12:47:47 +0100

    Fix strategies

--- argmining/strategies/strategies.py
+++ argmining/strategies/strategies.py
@@ -1,39 +1,39 @@
 from argmining.features.bag_of_words import BagOfWords
-import argmining.features.pos_distribution as pos_distribution
-import argmining.features.dependency_distribution as dependency_distribution
-import argmining.features.structural_features as structural_features
+from argmining.features.pos_distribution import POSDistribution
+from argmining.features.dependency_distribution import DependencyDistribution
+from argmining.features.structural_features import StructuralFeatures
 
 STRATEGIES = {'unigram': [BagOfWords(ngram=1).build()],
-              # 'unigram_lowercase': [bag_of_words.build(ngram=1, lowercase=True)],
-              # 'unigram_iwnlp': [bag_of_words.build(ngram=1, token_form='IWNLP_lemma')],
-              # 'unigram_iwnlp_lowercase': [bag_of_words.build(ngram=1, token_form='IWNLP_lemma', lowercase=True)],
-              # 'n_unigram': [bag_of_words.build(ngram=1, normalize=True)],
-              # 'n_unigram_lowercase': [bag_of_words.build(ngram=1, lowercase=True, normalize=True)],
-              # 'n_unigram_iwnlp': [bag_of_words.build(ngram=1, token_form='IWNLP_lemma', normalize=True)],
-              # 'n_unigram_iwnlp_lowercase': [
-              #     bag_of_words.build(ngram=1, token_form='IWNLP_lemma', lowercase=True, normalize=True)],
-              # 'bigram': [bag_of_words.build(ngram=2)],
-              # 'bigram_lowercase': [bag_of_words.build(ngram=2, lowercase=True)],
-              # 'bigram_iwnlp': [bag_of_words.build(ngram=2, token_form='IWNLP_lemma')],
-              # 'bigram_iwnlp_lowercase': [bag_of_words.build(ngram=2, token_form='IWNLP_lemma', lowercase=True)],
-              # 'n_bigram': [bag_of_words.build(ngram=2, normalize=True)],
-              # 'n_bigram_lowercase': [bag_of_words.build(ngram=2, lowercase=True, normalize=True)],
-              # 'n_bigram_iwnlp': [bag_of_words.build(ngram=2, token_form='IWNLP_lemma', normalize=True)],
-              # 'n_bigram_iwnlp_lowercase': [
-              #     bag_of_words.build(ngram=2, token_form='IWNLP_lemma', lowercase=True, normalize=True)],
-              # 'unigram_bigram': [bag_of_words.build(ngram=1, feature_name='unigram'),
-              #                    bag_of_words.build(ngram=2, feature_name='bigram')],
-              # 'pos_distribution': [pos_distribution.build()],
-              # 'pos_distribution_fs35': [pos_distribution.build(use_feature_selection=True, feature_selection_k=35)],
-              # 'pos_distribution_uts': [pos_distribution.build(use_STTS=False)],
-              # 'n_unigram+pos_distribution': [bag_of_words.build(ngram=1, normalize=True), pos_distribution.build()],
-              # 'dependency_distribution': [dependency_distribution.build()],
-              # 'dependency_distribution_fs10': [
-              #     dependency_distribution.build(use_feature_selection=True, feature_selection_k=10)],
-              # 'pos+dep_distribution': [pos_distribution.build(), dependency_distribution.build()],
-              # 'pos+dep_distribution_fs': [pos_distribution.build(use_feature_selection=True, feature_selection_k=35),
-              #                             dependency_distribution.build(use_feature_selection=True,
-              #                                                           feature_selection_k=10)],
-              # 'structural': [structural_features.build()],
-              # 'structural_without_token_length': [structural_features.build(use_sentence_length=False)]
+              'unigram_lowercase': [BagOfWords(ngram=1, lowercase=True).build()],
+              'unigram_iwnlp': [BagOfWords(ngram=1, token_form='IWNLP_lemma').build()],
+              'unigram_iwnlp_lowercase': [BagOfWords(ngram=1, token_form='IWNLP_lemma', lowercase=True).build()],
+              'n_unigram': [BagOfWords(ngram=1, normalize=True).build()],
+              'n_unigram_lowercase': [BagOfWords(ngram=1, lowercase=True, normalize=True).build()],
+              'n_unigram_iwnlp': [BagOfWords(ngram=1, token_form='IWNLP_lemma', normalize=True).build()],
+              'n_unigram_iwnlp_lowercase': [
+                  BagOfWords(ngram=1, token_form='IWNLP_lemma', lowercase=True, normalize=True).build()],
+              'bigram': [BagOfWords(ngram=2).build()],
+              'bigram_lowercase': [BagOfWords(ngram=2, lowercase=True).build()],
+              'bigram_iwnlp': [BagOfWords(ngram=2, token_form='IWNLP_lemma').build()],
+              'bigram_iwnlp_lowercase': [BagOfWords(ngram=2, token_form='IWNLP_lemma', lowercase=True).build()],
+              'n_bigram': [BagOfWords(ngram=2, normalize=True).build()],
+              'n_bigram_lowercase': [BagOfWords(ngram=2, lowercase=True, normalize=True).build()],
+              'n_bigram_iwnlp': [BagOfWords(ngram=2, token_form='IWNLP_lemma', normalize=True).build()],
+              'n_bigram_iwnlp_lowercase': [
+                  BagOfWords(ngram=2, token_form='IWNLP_lemma', lowercase=True, normalize=True).build()],
+              'unigram_bigram': [BagOfWords(ngram=1, feature_name='unigram').build(),
+                                 BagOfWords(ngram=2, feature_name='bigram').build()],
+              'pos_distribution': [POSDistribution().build()],
+              'pos_distribution_fs35': [POSDistribution(use_feature_selection=True, feature_selection_k=35).build()],
+              'pos_distribution_uts': [POSDistribution(use_STTS=False).build()],
+              'n_unigram+pos_distribution': [BagOfWords(ngram=1, normalize=True).build(), POSDistribution().build()],
+              'dependency_distribution': [DependencyDistribution().build()],
+              'dependency_distribution_fs10': [
+                  DependencyDistribution(use_feature_selection=True, feature_selection_k=10).build()],
+              'pos+dep_distribution': [POSDistribution().build(), StructuralFeatures().build()],
+              'pos+dep_distribution_fs': [POSDistribution(use_feature_selection=True, feature_selection_k=35).build(),
+                                          DependencyDistribution(use_feature_selection=True,
+                                                                 feature_selection_k=10).build()],
+              'structural': [StructuralFeatures().build()],
+              'structural_without_token_length': [StructuralFeatures(use_sentence_length=False).build()]
               }
