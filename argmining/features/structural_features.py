65f491e79185e7e7771288b9e6439d50dfac1666
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-20 13:46:07 +0100

    Refactor the structural feature

--- argmining/features/structural_features.py
+++ argmining/features/structural_features.py
@@ -10,6 +10,33 @@ def build(use_sentence_length=True):
     return ('structural_features', pipeline)
 
 
+def transform_sentence(thf_sentence, use_sentence_length=True):
+    values = []
+    words = list(map(lambda token: token.text, thf_sentence.tokens))
+    comma_relative = words.count(',') / float(len(words))
+    dot_relative = words.count('.') / float(len(words))
+    values.append(comma_relative)
+    values.append(dot_relative)
+    if use_sentence_length:
+        values.append(len(words))
+    link_count = 0
+    for word in words:
+        if word.startswith('www.') or word.startswith('http'):
+            link_count += 1
+    values.append(float(link_count))
+    last_word = words[len(words) - 1]
+
+    if last_word == '.':
+        values.extend([1.0, 0.0, 0.0, 0.0])
+    elif last_word == '!':
+        values.extend([0.0, 1.0, 0.0, 0.0])
+    elif last_word == '?':
+        values.extend([0.0, 0.0, 1.0, 0.0])
+    else:
+        values.extend([0.0, 0.0, 0.0, 1.0])
+    return values
+
+
 class StructuralFeatures(BaseEstimator):
     def __init__(self, use_sentence_length=True):
         self.logger = logging.getLogger()
@@ -21,41 +48,3 @@ class StructuralFeatures(BaseEstimator):
     def transform(self, X):
         transformed = list(map(lambda x: self.transform_sentence(x), X))
         return transformed
-
-    def transform_sentence(self, thf_sentence):
-        values = []
-        words = list(map(lambda token: token.text, thf_sentence.tokens))
-        comma_relative = words.count(',') / float(len(words))
-        dot_relative = words.count('.') / float(len(words))
-        values.append(comma_relative)
-        values.append(dot_relative)
-        if self.use_sentence_length:
-            values.append(len(words))
-        link_count = 0
-        for word in words:
-            if word.startswith('www.') or word.startswith('http'):
-                link_count += 1
-        values.append(float(link_count))
-        last_word = words[len(words) - 1]
-
-        if last_word == '.':
-            values.append(1.0)
-            values.append(0.0)
-            values.append(0.0)
-            values.append(0.0)
-        elif last_word == '!':
-            values.append(0.0)
-            values.append(1.0)
-            values.append(0.0)
-            values.append(0.0)
-        elif last_word == '?':
-            values.append(0.0)
-            values.append(0.0)
-            values.append(1.0)
-            values.append(0.0)
-        else:
-            values.append(0.0)
-            values.append(0.0)
-            values.append(0.0)
-            values.append(1.0)
-        return values
