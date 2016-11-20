90e2bee46f3cbf9126fe1068c5c437894ea92852
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-20 23:32:52 +0100

    Add more grid search parameters to the SVM

--- argmining/classifiers/svm.py
+++ argmining/classifiers/svm.py
@@ -3,9 +3,9 @@ from sklearn.svm import SVC
 
 
 def param_grid():
-    return {'classifier__C': [1e3],
-            'classifier__gamma': [0.005, 0.01]}
-    # return {'classifier__C': [1e3, 5e3, 1e4, 5e4, 1e5], 'classifier__gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
+    # return {'classifier__C': [1e3],
+    #        'classifier__gamma': [0.005, 0.01]}
+    return {'classifier__C': [1e3, 5e3, 1e4, 5e4, 1e5], 'classifier__gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
 
 
 def build(kernel='rbf', random_state=0, C=1.0, gamma='auto'):
