feae1be591915697371d273c5f6c56d155056ff6
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-20 23:05:27 +0100

    Adjust reduce_training_set to a list instead of a matrix

--- argmining/evaluation/reduce_training_set.py
+++ argmining/evaluation/reduce_training_set.py
@@ -1,18 +1,16 @@
 import logging
 
-
 def reduce_training_set(X_train, y_train, training_size):
     logger = logging.getLogger()
     if training_size == 100:
         return X_train, y_train
     training_size = float(training_size) / 100  # get percentage
     logger.debug('Reducing training size: {}'.format(training_size))
-    row_count = X_train.shape[0]
+    row_count = len(X_train)
     logger.debug('Row count before reduction: {}'.format(row_count))
     row_upper_index = int(round(row_count * training_size))
     logger.debug('Row_upper_index: {}'.format(row_upper_index))
-    row_indices = range(0, row_upper_index)
-    X_reduced = X_train[row_indices, :]
-    y_reduced = y_train[row_indices]
+    X_reduced = X_train[0: row_upper_index]
+    y_reduced = y_train[0: row_upper_index]
     logger.info("Training size {} => reducing {} rows to [0:{}]".format(training_size, row_count, row_upper_index))
     return X_reduced, y_reduced
