713684f304461bf81169bc577a6d91cb0e6c1697
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-20 22:54:52 +0100

    Improve logging

--- argmining/evaluation/shuffle.py
+++ argmining/evaluation/shuffle.py
@@ -4,9 +4,10 @@ import logging
 
 def shuffle_training_Set(X_train, y_train, random_state):
     logger = logging.getLogger()
-    logger.info("Shuffling with random_state: {}".format(random_state))
     if random_state is None:
+        logger.info("Using the original order of the dataset")
         return X_train, y_train
     else:
+        logger.info("Shuffling with random_state: {}".format(random_state))
         X_train, y_train = shuffle(X_train, y_train, random_state=random_state)
         return X_train, y_train
