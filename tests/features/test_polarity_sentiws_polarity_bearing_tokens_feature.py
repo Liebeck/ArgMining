66476b26e57e086e6782c7b7e80754240e09458d
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-21 16:50:42 +0100

    Adds test for 'SentiWSPolarityBearingTokens'

--- tests/features/test_polarity_sentiws_polarity_bearing_tokens_feature.py
+++ tests/features/test_polarity_sentiws_polarity_bearing_tokens_feature.py
@@ -1,38 +1,38 @@
 import unittest
-import argmining.features.sentiws_average_polarity_feature as polarity_sentiws_feature
+import argmining.features.sentiws_polarity_bearing_tokens_feature as sentiws_polarity_bearing_tokens_feature
 from argmining.models.thf_sentence_export import THFSentenceExport
 from argmining.models.token import Token
 
 
-class THFSentenceFeaturesStructural(unittest.TestCase):
-    def test_extract_average_polarity_example1(self):
+class THFSentenceSentiWSPolarityBearingTokens(unittest.TestCase):
+    def test_count_polarity_bearing_tokens_example1(self):
         tokens = []
         tokens.append(Token(1, None, None, None, None, None, None, 0.5))
         tokens.append(Token(2, None, None, None, None, None, None, None))
         tokens.append(Token(3, None, None, None, None, None, None, 1.5))
         thf_sentence = THFSentenceExport(None, None, None, tokens, None)
-        feature_value = polarity_sentiws_feature.extract_average_polarity(thf_sentence)
-        expected_value = [1.0]
+        feature_value = sentiws_polarity_bearing_tokens_feature.count_polarity_bearing_tokens(thf_sentence)
+        expected_value = [2]
         self.assertEqual(feature_value, expected_value)
 
-    def test_extract_average_polarity_example2(self):
+    def test_count_polarity_bearing_tokens_example2(self):
         tokens = []
         tokens.append(Token(1, None, None, None, None, None, None, None))
         tokens.append(Token(2, None, None, None, None, None, None, None))
         tokens.append(Token(3, None, None, None, None, None, None, None))
         thf_sentence = THFSentenceExport(None, None, None, tokens, None)
-        feature_value = polarity_sentiws_feature.extract_average_polarity(thf_sentence)
-        expected_value = [0.0]
+        feature_value = sentiws_polarity_bearing_tokens_feature.count_polarity_bearing_tokens(thf_sentence)
+        expected_value = [0]
         self.assertEqual(feature_value, expected_value)
 
-    def test_extract_average_polarity_example3(self):
+    def test_count_polarity_bearing_tokens_example3(self):
         tokens = []
         tokens.append(Token(1, None, None, None, None, None, None, None))
         tokens.append(Token(2, None, None, None, None, None, None, -1))
         tokens.append(Token(3, None, None, None, None, None, None, -1.5))
         thf_sentence = THFSentenceExport(None, None, None, tokens, None)
-        feature_value = polarity_sentiws_feature.extract_average_polarity(thf_sentence)
-        expected_value = [-1.25]
+        feature_value = sentiws_polarity_bearing_tokens_feature.count_polarity_bearing_tokens(thf_sentence)
+        expected_value = [2]
         self.assertEqual(feature_value, expected_value)
 
 
