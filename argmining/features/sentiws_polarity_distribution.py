b579e9ad0c8322ae225d1026cf25514bdb33318c
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-11-21 18:23:31 +0100

    Add SentiWS polarity distribution feature

---
+++ argmining/features/sentiws_polarity_distribution.py
@@ -0,0 +1,45 @@
+from sklearn.base import BaseEstimator
+from sklearn.pipeline import Pipeline
+import logging
+import numpy as np
+
+
+def build(bins=10):
+    pipeline = Pipeline([('transformer',
+                          SentiWSPolarityDistribution(bins=bins)),
+                         ])
+    return ('polarity_sentiws_distribution', pipeline)
+
+
+def extract_polarity_tokens(thf_sentence):
+    polarity_tokens = []
+    for token in thf_sentence.tokens:
+        if token.polarity is not None:
+            polarity_tokens.append(token.polarity)
+    if not polarity_tokens:
+        return [0]
+    else:
+        return polarity_tokens
+
+
+class SentiWSPolarityDistribution(BaseEstimator):
+    def __init__(self, bins=10):
+        self.bins = bins
+        self.logger = logging.getLogger()
+
+    def fit(self, X, y):
+        all_polarity_values = []
+        for thf_sentence in X:
+            all_polarity_values.extend(extract_polarity_tokens(thf_sentence))
+        histogram, edges = np.histogram(all_polarity_values, bins=self.bins)
+        self.edges = edges
+        return self
+
+    def transform(self, X):
+        transformed = list(map(lambda x: self.transform_sentence(x), X))
+        return transformed
+
+    def transform_sentence(self, thf_sentence):
+        polarity_tokens = extract_polarity_tokens(thf_sentence)
+        histogram, edges = np.histogram(polarity_tokens, bins=self.edges, density=False)
+        return histogram
