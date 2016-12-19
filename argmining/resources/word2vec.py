35b4c676ebb72eb9d551339e6a0ed9c4ea2bb812
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-12-19 16:20:41 +0100

    Add a method to annotate tokens with an embedding vector

--- argmining/resources/word2vec.py
+++ argmining/resources/word2vec.py
@@ -1,12 +1,29 @@
 import gensim
 from gensim.models import word2vec
-
+import logging
 
 class Word2Vec:
-    def __init__(self, model_path='data/word_embeddings/word2vec_wiki-de_20161120_100'):
+    def __init__(self, model_path='data/word_embeddings/word2vec_wiki-de_20161120_100', text_type='lowercase'):
         self.model_path = model_path
+        self.text_type = text_type
+        self.logger = logging.getLogger()
 
     def load(self):
-        model = gensim.models.Word2Vec.load_word2vec_format(self.model_path)
+        self.logger.info("Loading model: {}".format(self.model_path))
+        self.logger.info("text_type: {}".format(self.text_type))
+        self.model = gensim.models.Word2Vec.load_word2vec_format(self.model_path)
+        self.logger.info("Model loaded")
 
-    #def
+    def get_key(self, token):
+        if self.text_type == 'lowercase':
+            return token.text.lower()
+        return token.text
+
+    def annotate_sentence(self, sentence):
+        for token in sentence.tokens:
+            key = self.get_key(token)
+            if key in self.model.vocab:
+                token.embedding = self.model[key]
+
+    def annotate_sentences(self, sentences):
+        return list(map(lambda sentence: self.annotate_sentence(sentence), sentences))
