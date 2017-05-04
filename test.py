08a5d0a20a8bd07716ef1fb3d066bceeb71c6e55
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-04 13:10:50 +0200

    Remove unused file

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-from argmining.sentence.loaders.THF_sentence_corpus_loader import load
-
-taskA_train = load(file_path='data/THF/sentence/subtaskA_train.json')
-taskA_test = load(file_path='data/THF/sentence/subtaskA_test.json')
-taskB_train = load(file_path='data/THF/sentence/subtaskB_train.json')
-taskB_test = load(file_path='data/THF/sentence/subtaskB_test.json')
