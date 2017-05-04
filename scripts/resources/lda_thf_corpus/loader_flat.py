ed93b80f747874d32f352e1550edecbb42b3cf2e
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-04 16:15:37 +0200

    Load all comments

--- scripts/resources/lda_thf_corpus/loader_flat.py
+++ scripts/resources/lda_thf_corpus/loader_flat.py
@@ -19,11 +19,23 @@ class LoaderFlat(object):
             data = json.load(data_file)
             proposals = data["instance"]["tempelhofer-feld"]["proposals"]
             tokenized_documents = []
-            self.logger.info('Processing {} proposals'.format(len(proposals)))
-        # for each proposal
-        #   for each comment
-        #    recursive for each sub comment
+            # self.logger.info('Processing {} proposals'.format(len(proposals)))
+            all_comments = []
+            for proposal_id, proposal in proposals.items():
+                # self.logger.info('Processing proposal {}:'.format(proposal_id))
+                proposal_text = '{} {}'.format(proposal["title"], proposal["description"])
+                tokenized_documents.append(self.extract_tokens(proposal_text))
+                flattened_comments = self.flatten_comments(proposal["comments"])
+                # self.logger.info('Proposal {} has {} comments'.format(proposal_id, len(flattened_comments)))
+                all_comments.extend(flattened_comments)
+                # self.logger.info(proposal_text.encode('utf-8'))
+                # break
+            self.logger.info('Loaded {} proposals with {} comments'.format(len(proposals), len(all_comments)))
 
-    def flatten_proposal(self, proposal):
-        return None
+    def flatten_comments(self, comments):
+        all_comments = []
+        for comment_id, comment in comments.items():
+            all_comments.extend(self.flatten_comments(comment["comments"]))
+        all_comments.extend(comments)
+        return all_comments
 
