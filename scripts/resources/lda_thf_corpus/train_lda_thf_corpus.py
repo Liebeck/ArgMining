6515cd26e5d77448ce56fadf3d4383b83920e1bd
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-08 12:42:18 +0200

    Continue to work tokenizing the corpus

--- scripts/resources/lda_thf_corpus/train_lda_thf_corpus.py
+++ scripts/resources/lda_thf_corpus/train_lda_thf_corpus.py
@@ -1,7 +1,9 @@
 import argparse
 import logging
 from scripts.resources.lda_thf_corpus.loader_flat import LoaderFlat
+import spacy
 
+logger = logging.getLogger()
 
 
 def config_argparser():
@@ -13,8 +15,28 @@ def config_argparser():
     return argparser.parse_args()
 
 
+def extract_tokens(document, spacy_pipeline):
+    return None
+
+
+def load_thf_corpus_tokenized(path, spacy_pipeline):
+    tokenized_documents = []
+    thf_loader = LoaderFlat()
+    proposals = thf_loader.load_thf_corpus_flat(path)
+    for proposal in proposals:
+        proposal_text = '{} {}'.format(proposal["title"], proposal["description"])
+        tokenized_documents.append(extract_tokens(proposal_text, spacy_pipeline))
+        for comment in proposal["flat_comments"]:
+            tokenized_documents.append(extract_tokens(comment["text"], spacy_pipeline))
+
+    logger.info('Tokenized {} documents'.format(len(tokenized_documents)))
+    return tokenized_documents
+
+
 if __name__ == '__main__':
     arguments = config_argparser()
-    logger = logging.getLogger()
-    thf_loader = LoaderFlat()
-    tokenized_documents = thf_loader.load_thf_corpus_flat(arguments.corpuspath)
+    logger.info('Loading Spacy model')
+    # nlp = spacy.load('de')
+    nlp = None
+    logger.info('Spacy model loaded')
+    tokenized_documents = load_thf_corpus_tokenized(arguments.corpuspath, nlp)
