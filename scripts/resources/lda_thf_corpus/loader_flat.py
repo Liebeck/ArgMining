34786563f9b6f8fdc573a5bb91fd0cb2b58bbbbc
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-08 12:30:25 +0200

    Finished the loader and removed the tokenization out of the class

--- scripts/resources/lda_thf_corpus/loader_flat.py
+++ scripts/resources/lda_thf_corpus/loader_flat.py
@@ -1,47 +1,36 @@
 import logging
-import spacy
 import json
+
 
 class LoaderFlat(object):
     def __init__(self):
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
-        # self.logger.info('Loading Spacy model')
-        # self.nlp = spacy.load('de')
-        # self.logger.info('Spacy model loaded')
-
-    def extract_tokens(self, document):
-        return None
 
     def load_thf_corpus_flat(self, path):
         self.logger.info('Loading THF corpus from {}'.format(path))
         with open(path, encoding='utf-8') as data_file:
             data = json.load(data_file)
             proposals = data["instance"]["tempelhofer-feld"]["proposals"]
-            tokenized_documents = []
-            # self.logger.info('Processing {} proposals'.format(len(proposals)))
-            all_comments = []
+            proposals_flattened = []
+            comment_counter = 0
             for proposal_id, proposal in proposals.items():
-                # self.logger.info('Processing proposal {}:'.format(proposal_id))
-                proposal_text = '{} {}'.format(proposal["title"], proposal["description"])
-                tokenized_documents.append(self.extract_tokens(proposal_text))
-                flattened_comments = self.flatten_comments(proposal["comments"])
-                for comment in flattened_comments:
-                    self.logger.info(comment)
-                    break
-                    tokenized_documents.append(self.extract_tokens(comment["text"]))
-                # self.logger.info('Proposal {} has {} comments'.format(proposal_id, len(flattened_comments)))
-                all_comments.extend(flattened_comments)
-                # self.logger.info(proposal_text.encode('utf-8'))
-                # break
-            self.logger.info('Loaded {} proposals with {} comments'.format(len(proposals), len(all_comments)))
-            self.logger.info('Tokenized {} documents'.format(len(tokenized_documents)))
-            return tokenized_documents
+                proposal_object = {'title': proposal["title"],
+                                   'description': proposal["description"],
+                                   'proposal_id': proposal_id,
+                                   'flat_comments': self.flatten_comments(proposal["comments"])}
+                comment_counter += len(proposal_object['flat_comments'])
+                proposals_flattened.append(proposal_object)
+
+            self.logger.info('Loaded {} proposals with {} comments'.format(len(proposals), comment_counter))
+            return proposals_flattened
 
     def flatten_comments(self, comments):
         all_comments = []
         for comment_id, comment in comments.items():
-            all_comments.extend(self.flatten_comments(comment["comments"]))
-        all_comments.extend(comments)
+            subcomments = comment["comments"]
+            comment['comment_id'] = comment_id
+            comment.pop('comments', None)
+            all_comments.append(comment)
+            all_comments.extend(self.flatten_comments(subcomments))
         return all_comments
-
