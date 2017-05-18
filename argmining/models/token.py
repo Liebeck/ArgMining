b84a7eeb0857f9c4888398349a8fbf1b5b94ec9a
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-18 08:02:49 +0200

    Add more spacy fields to the token class

--- argmining/models/token.py
+++ argmining/models/token.py
@@ -2,7 +2,7 @@ class Token:
     def __init__(self, token_index_in_sentence, text, pos_tag=None, mate_tools_pos_tag=None, mate_tools_lemma=None,
                  tree_tagger_lemma=None,
                  iwnlp_lemma=None, polarity=None, spacy_pos_stts=None, spacy_pos_uts=None, spacy_ner_type=None,
-                 spacy_ner_iob=None):
+                 spacy_ner_iob=None, spacy_shape=None, spacy_is_punct=None, spacy_like_url=None, spacy_like_num=None):
         self.token_index_in_sentence = token_index_in_sentence
         self.text = text
         self.pos_tag = pos_tag
@@ -16,6 +16,10 @@ class Token:
         self.spacy_pos_uts = spacy_pos_uts
         self.spacy_ner_type = spacy_ner_type
         self.spacy_ner_iob = spacy_ner_iob
+        self.spacy_shape = spacy_shape
+        self.spacy_is_punct = spacy_is_punct
+        self.spacy_like_url = spacy_like_url
+        self.spacy_like_num = spacy_like_num
 
     def get_key(self, text_type):
         if text_type == 'lowercase':
