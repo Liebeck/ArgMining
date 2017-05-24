2f7f1eb883a907f27e59aa76a191c0b5b80cc868
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-24 18:49:28 +0200

    Change spaCy's pipeline

--- scripts/nlp/spacy_wrapper.py
+++ scripts/nlp/spacy_wrapper.py
@@ -2,17 +2,28 @@ import spacy
 import logging
 from argmining.models.token import Token
 from argmining.models.dependency import Dependency
+from iwnlp.iwnlp_wrapper import IWNLPWrapper
+from iwnlp_lemmatizer_spacy import IWNLPLemmatizerSpacy
 
 logger = logging.getLogger()
+
+
+# [self.tagger, self.parser, self.matcher, self.entity]
+
+# def arbitrary_fixup_rules(doc):
 
 
 class SpacyWrapper(object):
     def __init__(self):
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
+        # self.lemmatizer = IWNLPWrapper(lemmatizer_path='data/IWNLP/IWNLP.Lemmatizer_20170501.json')
         self.logger.debug('Loading Spacy model')
         self.nlp = spacy.load('de')
+        self.nlp.pipeline = [self.nlp.tagger, self.nlp.parser, self.nlp.matcher, self.nlp.entity,
+                             IWNLPLemmatizerSpacy.load(lemmatizer_path='data/IWNLP/IWNLP.Lemmatizer_20170501.json')]
         self.logger.debug('Spacy model loaded')
+
         # todo: load sentiws polarity
 
     def process_sentence(self, sentence):
@@ -35,8 +46,8 @@ class SpacyWrapper(object):
             tokens.append(token_model)
             dependency_model = Dependency(token.i + 1, token.dep_, token.head.i + 1)
             dependencies.append(dependency_model)
-            # print(token_model.token_index_in_sentence, token_model.text.encode('utf-8'), token_model.spacy_pos_stts,
-            # token_model.spacy_pos_uts, token_model.spacy_ner_type, token_model.spacy_ner_iob)
+            print(token_model.token_index_in_sentence, token_model.text.encode('utf-8'), token_model.spacy_pos_stts,
+                  token_model.spacy_pos_uts, token_model.spacy_ner_type, token_model.spacy_ner_iob, token_model.iwnlp_lemma)
             # print(token.i + 1, token.dep_, token.head.i + 1, token.head.text.encode('utf-8'))
         return {
             'tokens': tokens,
@@ -46,4 +57,5 @@ class SpacyWrapper(object):
 
 if __name__ == '__main__':
     spacy_wrapper = SpacyWrapper()
-    spacy_wrapper.process_sentence('Das ist ein schöner Testsatz mit schlechter Bewertung.')
+    spacy_wrapper.process_sentence('Das ist ein guter Testsatz mit schlechter Bewertung.')
+    #spacy_wrapper.process_sentence('Das ist ein schöner Testsatz mit schlechter Bewertung.')
