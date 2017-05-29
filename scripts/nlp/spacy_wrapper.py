214cbd70b090720b1829f1985e330ac25387377a
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-29 11:04:16 +0200

    Add IWNLP to spacy wrapper script

--- scripts/nlp/spacy_wrapper.py
+++ scripts/nlp/spacy_wrapper.py
@@ -12,16 +12,19 @@ logger = logging.getLogger()
 
 # def arbitrary_fixup_rules(doc):
 
+def format_iwnlp_lemma(input):
+    if input is None:
+        return ''
+    else:
+        return [lemma.encode('utf-8') for lemma in input]
 
 class SpacyWrapper(object):
     def __init__(self):
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
-        # self.lemmatizer = IWNLPWrapper(lemmatizer_path='data/IWNLP/IWNLP.Lemmatizer_20170501.json')
+        self.lemmatizer = IWNLPWrapper(lemmatizer_path='data/IWNLP/IWNLP.Lemmatizer_20170501.json')
         self.logger.debug('Loading Spacy model')
         self.nlp = spacy.load('de')
-        self.nlp.pipeline = [self.nlp.tagger, self.nlp.parser, self.nlp.matcher, self.nlp.entity,
-                             IWNLPLemmatizerSpacy.load(lemmatizer_path='data/IWNLP/IWNLP.Lemmatizer_20170501.json')]
         self.logger.debug('Spacy model loaded')
 
         # todo: load sentiws polarity
@@ -31,23 +34,25 @@ class SpacyWrapper(object):
         tokens = []
         dependencies = []
         for token in result:
+            iwnlp_lemma = self.lemmatizer.lemmatize(token.text, pos_universal_google=token.pos_)
             token_model = Token(token.i + 1,
                                 text=token.text,
                                 spacy_pos_stts=token.pos_,
                                 spacy_pos_universal_google=token.tag_,
+                                iwnlp_lemma=iwnlp_lemma,
                                 spacy_ner_type=token.ent_type_,
                                 spacy_ner_iob=token.ent_iob_,
                                 spacy_is_punct=token.is_punct,
                                 spacy_like_num=token.like_num,
                                 spacy_like_url=token.like_url,
                                 spacy_shape=token.shape_)
-            # Todo: add IWNLP Lemma
             # Todo: Add SentiWS polarity
             tokens.append(token_model)
             dependency_model = Dependency(token.i + 1, token.dep_, token.head.i + 1)
             dependencies.append(dependency_model)
-            print(token_model.token_index_in_sentence, token_model.text.encode('utf-8'), token_model.spacy_pos_stts,
-                  token_model.spacy_pos_uts, token_model.spacy_ner_type, token_model.spacy_ner_iob,
+            print(token_model.token_index_in_sentence, token_model.text.encode('utf-8'),
+                  format_iwnlp_lemma(token_model.iwnlp_lemma), token_model.spacy_pos_stts,
+                  token_model.spacy_pos_universal_google, token_model.spacy_ner_type, token_model.spacy_ner_iob,
                   token_model.iwnlp_lemma)
             # print(token.i + 1, token.dep_, token.head.i + 1, token.head.text.encode('utf-8'))
         return {
