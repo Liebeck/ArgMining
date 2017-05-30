64ef4c221ae8cb786a45ce02dbe3b6da3d5c9727
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-30 13:04:34 +0200

    Add textdepth to sentence model

--- argmining/models/thf_sentence_export.py
+++ argmining/models/thf_sentence_export.py
@@ -1,7 +1,8 @@
 class THFSentenceExport:
-    def __init__(self, uniqueID, label, text, tokens, dependencies):
+    def __init__(self, uniqueID, label, text, tokens, dependencies, textdepth):
         self.uniqueID = uniqueID
         self.label = label
         self.text = text
         self.tokens = tokens
         self.dependencies = dependencies
+        self.textdepth = textdepth
