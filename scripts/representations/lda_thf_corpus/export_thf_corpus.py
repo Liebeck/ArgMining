5f56ede6753eb2ee088544e860ad10e8aaf2dae8
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-06-07 14:00:58 +0200

    export_thf_corpus now uses SpacyWrapper

--- scripts/representations/lda_thf_corpus/export_thf_corpus.py
+++ scripts/representations/lda_thf_corpus/export_thf_corpus.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import logging
 import spacy
-
+from scripts.nlp.spacy_wrapper import SpacyWrapper
 from scripts.representations.lda_thf_corpus.loader_flat import LoaderFlat
 
 logger = logging.getLogger()
@@ -17,10 +17,10 @@ def config_argparser():
     return argparser.parse_args()
 
 
-def extract_tokens(document, spacy_pipeline):
+def extract_tokens(document, spacy):
     tokens = []
-    for i, token in enumerate(spacy_pipeline(document)):
-        if not token.is_punct and not token.is_space and '\r\n' not in token.text:
+    for i, token in enumerate(spacy.process_sentence(document)['tokens']):
+        if not token.spacy_is_punct and not token.spacy_is_space and '\r\n' not in token.text:
             tokens.append(token)
     return tokens
 
@@ -29,25 +29,26 @@ def tokens_to_json(tokens):
     json_tokens = []
     for token in tokens:
         json_tokens.append({'Text': token.text,
-                            'POS': token.pos_,
-                            'NER_type': token.ent_type_,
-                            'NER_IOB': token.ent_iob_
+                            'Lemma': token.iwnlp_lemma,
+                            'POS': token.spacy_pos_universal_google,
+                            'NER_type': token.spacy_ner_type,
+                            'NER_IOB': token.spacy_ner_iob
                             })
     return json_tokens
 
 
-def load_thf_corpus_tokenized(path, spacy_pipeline):
+def load_thf_corpus_tokenized(path, spacy):
     documents = []
     thf_loader = LoaderFlat()
     proposals = thf_loader.load_thf_corpus_flat(path)
     for proposal in proposals:
         proposal_text = '{} {}'.format(proposal["title"], proposal["description"])
         documents.append({'Text': proposal_text,
-                          'Tokens': tokens_to_json(extract_tokens(proposal_text, spacy_pipeline))
+                          'Tokens': tokens_to_json(extract_tokens(proposal_text, spacy))
                           })
         for comment in proposal["flat_comments"]:
             documents.append({'Text': comment["text"],
-                              'Tokens': tokens_to_json(extract_tokens(comment["text"], spacy_pipeline))
+                              'Tokens': tokens_to_json(extract_tokens(comment["text"], spacy))
                               })
     logger.info('Tokenized {} documents'.format(len(documents)))
     return documents
@@ -55,10 +56,8 @@ def load_thf_corpus_tokenized(path, spacy_pipeline):
 
 if __name__ == '__main__':
     arguments = config_argparser()
-    logger.info('Loading Spacy model')
-    nlp = spacy.load('de')
-    logger.info('Spacy model loaded')
-    tokenized_documents = load_thf_corpus_tokenized(arguments.corpuspath, nlp)
+    spacy = SpacyWrapper()
+    tokenized_documents = load_thf_corpus_tokenized(arguments.corpuspath, spacy)
     output_path = 'data/THF/corpus_tokenized-2015-07-07.json'
     logger.info('Writing tokenized corpus to: {}'.format(output_path))
     with open(output_path, 'w') as outfile:
