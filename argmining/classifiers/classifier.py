fab83a9a1deec93d613d69b9aae8fb09b05e1989
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-09-18 19:39:12 +0200

    SVM mit linearem Kernel hinzugefügt

--- argmining/classifiers/classifier.py
+++ argmining/classifiers/classifier.py
@@ -6,6 +6,8 @@ from argmining.classifiers import random_forest
 def get_classifier(name, cross_validation=False):
     if name == 'svm':
         return svm.build(), svm.param_grid(cross_validation=cross_validation)
+    if name == 'svm-linear':
+        return svm.build(kernel='linear'), svm.param_grid_linear(cross_validation=cross_validation)
     elif name == 'knn':
         return knn.build(), knn.param_grid(cross_validation=cross_validation)
     elif name == 'rf':
@@ -16,6 +18,10 @@ def get_classifier(name, cross_validation=False):
 
 def create_classifier(name, classifier_params):
     if name == 'svm':
+        return svm.build(**classifier_params)
+    elif name == 'svm-linear':
+        classifier_params['kernel'] = 'linear'
+        print(classifier_params)
         return svm.build(**classifier_params)
     elif name == 'knn':
         return knn.build(**classifier_params)
--- argmining/classifiers/svm.py
+++ argmining/classifiers/svm.py
@@ -10,6 +10,12 @@ def param_grid(cross_validation=False):
         return {'classifier__C': [1e3, 5e3, 1e4, 5e4, 1e5],
                 'classifier__gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
 
+def param_grid_linear(cross_validation=False):
+    if cross_validation:
+        return {'classifier__C': [1e3]}
+    else:
+        return {'classifier__C': [1e3, 5e3, 1e4, 5e4, 1e5]}
+
 
 def build(kernel='rbf', random_state=0, C=1.0, gamma='auto'):
     return SVC(kernel=kernel, class_weight='balanced', random_state=random_state, C=C, gamma=gamma)
