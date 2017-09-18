c8a378681346bd234bcf610ba37828dac242b815
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-09-18 12:32:52 +0200

    Add more grid search parameters to the build function

--- argmining/classifiers/random_forest.py
+++ argmining/classifiers/random_forest.py
@@ -16,5 +16,9 @@ def param_grid(cross_validation=False):
                 "classifier__criterion": ["gini", "entropy"]}
 
 
-def build(random_state=0, n_jobs=1):
-    return RandomForestClassifier(class_weight='balanced', random_state=random_state, n_jobs=n_jobs)
+def build(random_state=0, n_jobs=1, min_samples_split=2, n_estimators=10, max_depth=None, max_features='auto',
+          min_samples_leaf=1, bootstrap=True, criterion='gini'):
+    return RandomForestClassifier(class_weight='balanced', random_state=random_state, n_jobs=n_jobs,
+                                  min_samples_split=min_samples_split, n_estimators=n_estimators, max_depth=max_depth,
+                                  max_features=max_features, min_samples_leaf=min_samples_leaf, bootstrap=bootstrap,
+                                  criterion=criterion)
