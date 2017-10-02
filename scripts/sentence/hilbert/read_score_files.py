15fe8c20b9e63c3389945084ebac23694d59aa93
Liebeck <Liebeck@users.noreply.github.com>
2017-10-02 16:57:43 +0200

    Expand the result script to create a LaTeX table

--- scripts/sentence/hilbert/read_score_files.py
+++ scripts/sentence/hilbert/read_score_files.py
@@ -18,7 +18,7 @@ def read_all_score_files(jobarray):
             f1_scores = []
             for line in file_handler:
                 if line.startswith('Micro-averaged F1: '):
-                    f1_mean = line[len('Micro-averaged F1: '):-1]
+                    f1_mean = float(line[len('Micro-averaged F1: '):-1])
                 if line.startswith('Individual scores: [ '):
                     f1_scores_raw = line[len('Individual scores: [ '):-2]
                     # print(f1_scores_raw)
@@ -81,13 +81,118 @@ def print_sorted_results(subtask):
     print('\n\n\n\n\n\n')
 
 
-if __name__ == '__main__':
-    jobarray = read_jobarray()
-    finished_evaluations = read_all_score_files(jobarray)
-    subtask_A, subtask_B, subtask_C = group_results_by_subtask(finished_evaluations)
+def print_results_all_subtasks(subtask_A, subtask_B, subtask_C):
     print('Subtask A: {} jobs finished'.format(len(subtask_A)))
     print_sorted_results(subtask_A)
     print('Subtask B: {} jobs finished'.format(len(subtask_B)))
     print_sorted_results(subtask_B)
     print('Subtask C: {} jobs finished'.format(len(subtask_C)))
     print_sorted_results(subtask_C)
+
+
+def filter_feature_and_classifier(subtask_results, feature, classifier):
+    filtered = []
+    for result in subtask_results:
+        if result['classifier'] == classifier and result['strategy'] == feature:
+            filtered.append(result)
+    return filtered
+
+
+def filter_by_lda_path(results, lda_path):
+    filtered = []
+    for result in results:
+        if any(lda_path in s for s in result['other_params']):
+            filtered.append(result)
+    return filtered
+
+
+def group_by_feature_and_classifier(subtask_results):
+    result_dict = {}
+    classifiers = ['svm', 'svm-linear', 'knn', 'rf']
+    features = ['unigram', 'bigram', 'character_ngrams', 'grammatical_spacy', 'pos_distribution_spacy',
+                'dependency_distribution_spacy', 'unigram+grammatical_spacy', 'n_unigram+shape', 'sentiws_distribution',
+                'embedding_centroid_100', 'embedding_centroid_200', 'embedding_centroid_300',
+                'character_embeddings_centroid_100', 'unigram+embedding_centroid_100',
+                'unigram+embedding_centroid_200', 'unigram+embedding_centroid_300',
+                'unigram+character_embeddings_centroid_100']
+    for classifier in classifiers:
+        for feature in features:
+            key = '{}_{}'.format(classifier, feature)
+            results_for_combination = filter_feature_and_classifier(subtask_results, feature, classifier)
+            if results_for_combination:
+                best_result = max(results_for_combination, key=lambda d: d['f1_mean'])
+                result_dict[key] = best_result
+            else:
+                result_dict[key] = []
+                # result_dict[key] = results_for_combination
+
+    for classifier in classifiers:
+        for feature in ['lda_distribution', 'unigram+lda_distribution']:
+            for lda_path in ['thf', 'wikipedia']:
+                results_for_combination = filter_feature_and_classifier(subtask_results, feature, classifier)
+                results_for_combination = filter_by_lda_path(results_for_combination, lda_path)
+                key = '{}_{}_{}'.format(classifier, feature, lda_path)
+                if results_for_combination:
+                    best_result = max(results_for_combination, key=lambda d: d['f1_mean'])
+                    result_dict[key] = best_result
+                else:
+                    result_dict[key] = []
+                    # result_dict[key] = results_for_combination
+    return result_dict
+
+
+def print_table_subtask(all_rows, best_subtask_1, best_subtask_2=None):
+    classifiers = ['svm', 'svm-linear', 'knn', 'rf']
+    header_row = [' '.ljust(40)]
+    column_ljust = 7
+    for classifier in classifiers:
+        header_row.append(classifier.ljust(column_ljust))
+    if best_subtask_2:
+        header_row.extend('&')
+        for classifier in classifiers:
+            header_row.append(classifier.ljust(column_ljust))
+    print('\t& '.join(header_row) + '\\\\')
+    for row in all_rows:
+        columns = [row.ljust(40)]
+        for classifier in classifiers:
+            key = '{}_{}'.format(classifier, row)
+            cell = "X".ljust(column_ljust)
+            if best_subtask_1[key]:
+                cell = "{:.2f}".format(best_subtask_1[key]['f1_mean']).ljust(column_ljust)
+            columns.append(cell)
+        if best_subtask_2:
+            columns.append('&')
+            for classifier in classifiers:
+                key = '{}_{}'.format(classifier, row)
+                cell = "X".ljust(column_ljust)
+                if best_subtask_2[key]:
+                    cell = "{:.2f}".format(best_subtask_2[key]['f1_mean']).ljust(column_ljust)
+                columns.append(cell)
+        print('\t& '.join(columns) + '\\\\')
+
+
+def print_tables(subtask_A, subtask_B, subtask_C):
+    best_subtask_A = group_by_feature_and_classifier(subtask_A)
+    best_subtask_B = group_by_feature_and_classifier(subtask_B)
+    best_subtask_C = group_by_feature_and_classifier(subtask_C)
+    all_rows = ['unigram', 'bigram', 'pos_distribution_spacy', 'dependency_distribution_spacy',
+                'grammatical_spacy',
+                'n_unigram+shape', 'lda_distribution_wikipedia', 'lda_distribution_thf', 'character_ngrams',
+                'embedding_centroid_100', 'embedding_centroid_200', 'embedding_centroid_300', 'sentiws_distribution',
+                'character_embeddings_centroid_100', 'unigram+grammatical_spacy', 'unigram+lda_distribution_thf',
+                'unigram+lda_distribution_wikipedia', 'unigram+embedding_centroid_100',
+                'unigram+embedding_centroid_200', 'unigram+embedding_centroid_300',
+                'unigram+character_embeddings_centroid_100']
+    print('*****\nSubtask A+B')
+    print_table_subtask(all_rows, best_subtask_A, best_subtask_B)
+    print('*****\nSubtask C')
+    print_table_subtask(all_rows, best_subtask_C)
+
+
+if __name__ == '__main__':
+    jobarray = read_jobarray()
+    finished_evaluations = read_all_score_files(jobarray)
+    subtask_A, subtask_B, subtask_C = group_results_by_subtask(finished_evaluations)
+    print_tables(subtask_A, subtask_B, subtask_C)
+    print('\n\n\n\n')
+    print_results_all_subtasks(subtask_A, subtask_B, subtask_C)
