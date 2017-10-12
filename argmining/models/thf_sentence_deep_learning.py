9aced4ad44fdf899b21c73fe802106b725dace37
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-10-12 20:14:01 +0200

    Start to work on a deep learning loader

---
+++ argmining/models/thf_sentence_deep_learning.py
@@ -0,0 +1,6 @@
+class THFSentenceDeepLearning:
+    def __init__(self, uniqueID, label, text, tokens):
+        self.uniqueID = uniqueID
+        self.label = label
+        self.text = text
+        self.tokens = tokens
