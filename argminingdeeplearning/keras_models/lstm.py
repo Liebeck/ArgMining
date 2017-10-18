b445fd4358bdedd78f1d96f3bd9359c0deb2858d
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-10-18 14:46:18 +0200

    Update keras model

--- argminingdeeplearning/keras_models/lstm.py
+++ argminingdeeplearning/keras_models/lstm.py
@@ -7,11 +7,11 @@ def lstm_embedding_empty(number_of_classes, max_features=20000):
     model = Sequential()
     model.add(Embedding(max_features, 128))
     model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-    model.add(Dense(1, activation='sigmoid'))
+    # model.add(Dense(1, activation='sigmoid'))
     model.add(Dense(number_of_classes))
     model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  # loss='binary_crossentropy',
+    model.compile(# loss='categorical_crossentropy',
+                  loss='binary_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
     return model
