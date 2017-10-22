dbb64b28e48c3e24e25f6919a4024d36e10c72d3
Liebeck <Liebeck@users.noreply.github.com>
2017-10-22 19:45:16 +0200

    Add caches for embeddings

--- scripts/deep_learning/README.md
+++ scripts/deep_learning/README.md
@@ -1,4 +1,17 @@
+# dev.py
+
 Local
 ``` bash
 python3 scripts/deep_learning/dev.py -subtask A -padding_length 20
 ```
+
+# create_embedding_cache
+``` bash
+python3 scripts/deep_learning/create_embedding_cache.py -embedding_type word2vec -embedding_path /home/matthias/shared/word2vec/word2vec_wiki-de_20170501_300_binary -embedding_cache_name word2vec_wiki_de_20170501_300-reduced
+python3 scripts/deep_learning/create_embedding_cache.py -embedding_type fasttext -embedding_path /home/matthias/shared/fasttext/dewiki-20170501-3_6-10 -embedding_cache_name fasttext_dewiki-20170501-3_6-10-reduced
+```
+
+
+
+Word2vec cache: 6188/7065 words
+fastText cache: 6890/7065 words
---
+++ scripts/deep_learning/create_embedding_cache.py
@@ -0,0 +1,67 @@
+from argminingdeeplearning.loaders import vocabulary_builder
+import argparse
+import gensim
+import logging
+import pickle
+from gensim.models.wrappers.fasttext import FastText as FT_wrapper
+# from gensim.models.fasttext import FastText as FT_gensim
+
+# The usage of an embedding cache is inspired by https://github.com/UKPLab/argument-reasoning-comprehension-task/
+
+def config_argparser():
+    argparser = argparse.ArgumentParser(description='Create smaller ArgMining Deep Learning')
+    argparser.add_argument('-embedding_type', type=str, required=True)
+    argparser.add_argument('-embedding_path', type=str, required=True)
+    argparser.add_argument('-embedding_cache_name', type=str, required=True)
+    return argparser.parse_args()
+
+
+def reduce_word2vec_embedding(word2vec_path, words):
+    model = gensim.models.KeyedVectors.load(word2vec_path)
+    word_to_embedding = {}
+    coverage = 0
+    for word in words:
+        key = word.lower()
+        if word in model.wv.vocab:
+            coverage = coverage + 1
+            word_to_embedding[key] = model[key]
+        else:
+            word_to_embedding[key] = None
+    print('Word2vec cache: {}/{} words'.format(coverage, len(words)))
+    return word_to_embedding
+
+
+def reduce_fasttext_embedding(fasttext_path, words):
+    model = FT_wrapper.load(fasttext_path)
+    print(model)
+    word_to_embedding = {}
+    coverage = 0
+    for word in words:
+        key = word.lower()
+        if word in model:
+            coverage = coverage + 1
+            word_to_embedding[key] = model[key]
+        else:
+            word_to_embedding[key] = None
+    print('fastText cache: {}/{} words'.format(coverage, len(words)))
+    return word_to_embedding
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    arguments = config_argparser()
+
+    subtask_A_train_path = 'data/THF/sentence/subtaskA_v3_train.json'
+    subtask_B_train_path = 'data/THF/sentence/subtaskB_v3_train.json'
+    words_A = list(vocabulary_builder.get_word_frequencies(subtask_A_train_path).keys())
+    words_B = list(vocabulary_builder.get_word_frequencies(subtask_B_train_path).keys())
+    words = list(set().union(words_A, words_B))
+    embedding_cache = None
+    if arguments.embedding_type == 'word2vec':
+        embedding_cache = reduce_word2vec_embedding(arguments.embedding_path, words)
+    elif arguments.embedding_type == 'fasttext':
+        embedding_cache = reduce_fasttext_embedding(arguments.embedding_path, words)
+    else:
+        raise ValueError("embedding type not supported")
+    embedding_cache_path = 'data/embedding_cache/{}'.format(arguments.embedding_cache_name)
+    pickle.dump(embedding_cache, open(embedding_cache_path, 'wb'))
