271dd5af6e7b1e7421cb3e55186e7608a593e4d7
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-10-24 16:50:34 +0200

    Code cleanup and saving models

---
+++ argminingdeeplearning/utils.py
@@ -0,0 +1,16 @@
+from pandas_confusion import ConfusionMatrix
+
+
+def write_prediction_file(path, test_unique_ids, Y_test_indices, y_prediction_classes):
+    with open(path, 'w') as prediction_handler:
+        prediction_handler.write('{}\t{}\t{}\n'.format("UniqueID", "Gold_Label", "Prediction"))
+        for index, val in enumerate(test_unique_ids):
+            prediction_handler.write('{}\t{}\t{}\n'.format(val, Y_test_indices[index], y_prediction_classes[index]))
+
+
+def write_score_file(score_file, f1_mean, f1, Y_test_indices, y_prediction_classes):
+    with open(score_file, 'w') as score_handler:
+        score_handler.write("Micro-averaged F1: {}\n".format(f1_mean))
+        score_handler.write("Individual scores: {}\n".format(f1))
+        score_handler.write("Confusion matrix:\n")
+        score_handler.write(str(ConfusionMatrix(Y_test_indices, y_prediction_classes)))
--- scripts/deep_learning/dev.py
+++ scripts/deep_learning/dev.py
@@ -5,10 +5,12 @@ import logging
 import time
 import numpy as np
 from argminingdeeplearning.keras_models import lstm
-from sklearn.metrics import f1_score
 from pandas_confusion import ConfusionMatrix
+from sklearn.metrics import f1_score
+from keras.callbacks import ModelCheckpoint
 import pickle
 import sys
+from argminingdeeplearning import utils
 
 
 def config_logger(log_level=logging.INFO):
@@ -39,6 +41,7 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     np.random.seed(14021993)
     arguments = config_argparser()
+    # Step 1) Load dataset
     train_path = 'data/THF/sentence/subtask{}_v3_train.json'.format(arguments.subtask)
     test_path = 'data/THF/sentence/subtask{}_v3_test.json'.format(arguments.subtask)
     number_of_classes = 2 if arguments.subtask == 'A' else 3
@@ -47,7 +50,6 @@ if __name__ == '__main__':
         embedding_cache_path = 'data/embedding_cache/{}'.format(arguments.embedding_cache_name)
         logger.info('Loading embedding cache: {}'.format(embedding_cache_path))
         embedding_cache = pickle.load(open(embedding_cache_path, "rb"))
-        # print(embedding_cache)
         logger.info('Embedding cache loaded')
     logger.debug('Create mapping')
     word_to_index_mapping, index_to_embedding_mapping = vocabulary_builder.create_mappings(train_path, embedding_cache)
@@ -63,28 +65,39 @@ if __name__ == '__main__':
     # logger.info(Y_train)
     # logger.info(Y_train.shape)
 
+    # Step 2) Create model
     # model = lstm.lstm_embedding_empty(number_of_classes)
     # model = lstm.lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping,
                                            # input_length=arguments.padding_length)
     # model = lstm.lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping,
                                                 # input_length=arguments.padding_length)
-    model = lstm.blstm(number_of_classes, index_to_embedding_mapping,input_length=arguments.padding_length)
+    model = lstm.blstm(number_of_classes, index_to_embedding_mapping, input_length=arguments.padding_length)
 
+    # Step 3) Train the model
     logger.info('Train...')
+    model_save_path = 'results/sentence_deeplearning/temp/{}_{}_{}'.format(arguments.subtask, arguments.kerasmodel,
+                                                                           time.strftime('%Y%m%d_%H%M%S'))
+    checkpoint_save_path = model_save_path + "{epoch:03d}-{val_acc:.4f}.hdf5"
+    checkpoint = ModelCheckpoint(checkpoint_save_path,
+                                 monitor='val_acc', verbose=1,
+                                 save_best_only=True, mode='auto')
     model.fit(X_train, Y_train,
               batch_size=arguments.batch_size,
               epochs=arguments.epochs,
               verbose=1,
+              callbacks=[checkpoint],
               validation_data=(X_test, Y_test))
+
+    # Step 4) Save the last model
+    model.save(model_save_path + "_last.hdf5")
+    # Step 5) Predict the test set
     score, acc = model.evaluate(X_test, Y_test, batch_size=arguments.batch_size)
-
     y_prediction = model.predict(X_test, batch_size=arguments.batch_size)
-
     y_prediction_classes = np.argmax(y_prediction, axis=1)
+    # Step 6) Print results
     logger.info(y_prediction_classes)
     logger.info('Test score: {}'.format(score))
     logger.info('Test accuracy: {}'.format(acc))
-
     f1 = f1_score(Y_test_indices, y_prediction_classes, average=None)
     f1_mean = np.mean(f1)
     logger.info("Macro-averaged F1: {}".format(f1_mean))
@@ -96,17 +109,11 @@ if __name__ == '__main__':
                                                                             arguments.kerasmodel,
                                                                             time_string)
 
-    prediction_file = output_path_base + '.predictions'
-    with open(prediction_file, 'w') as prediction_handler:
-        prediction_handler.write('{}\t{}\t{}\n'.format("UniqueID", "Gold_Label", "Prediction"))
-        for index, val in enumerate(test_unique_ids):
-            prediction_handler.write('{}\t{}\t{}\n'.format(val, Y_test_indices[index], y_prediction_classes[index]))
-    score_file = output_path_base + '.score'
-    with open(score_file, 'w') as score_handler:
-        score_handler.write("Micro-averaged F1: {}\n".format(f1_mean))
-        score_handler.write("Individual scores: {}\n".format(f1))
-        score_handler.write("Confusion matrix:\n")
-        score_handler.write(str(ConfusionMatrix(Y_test_indices, y_prediction_classes)))
+    # Step 7) Print results to the file system
+    utils.write_prediction_file(path=output_path_base + '.predictions', test_unique_ids=test_unique_ids,
+                                Y_test_indices=Y_test_indices, y_prediction_classes=y_prediction_classes)
+    utils.write_score_file(score_file=output_path_base + '.score', f1_mean=f1_mean, f1=f1,
+                           Y_test_indices=Y_test_indices, y_prediction_classes=y_prediction_classes)
 
     print("Total execution time in %0.3fs" % (time.time() - t0))
     print("*****************************************")
