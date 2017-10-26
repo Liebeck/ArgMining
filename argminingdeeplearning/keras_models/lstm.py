fb508e6214812b9b3fa9b634c63f0aadeaa3d88f
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-10-26 18:56:57 +0200

    Refactor values into parameters

--- argminingdeeplearning/keras_models/lstm.py
+++ argminingdeeplearning/keras_models/lstm.py
@@ -4,6 +4,7 @@ from keras.layers import LSTM, Bidirectional
 from theano.scalar import float32
 import numpy as np
 
+
 def lstm_embedding_empty(number_of_classes, max_features=7000, lstm_size=128, dropout=0.2):
     print('lstm_embedding_empty called')
     model = Sequential()
@@ -16,7 +17,9 @@ def lstm_embedding_empty(number_of_classes, max_features=7000, lstm_size=128, dr
                   metrics=['accuracy'])
     return model
 
-def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, padding_length):
+
+def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, padding_length,
+                              lstm_dimensionality=128, dropout=0.2, recurrent_dropout=0.2):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
     print('keys.len: {}'.format(number_of_words))
@@ -29,7 +32,7 @@ def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, pad
                                 weights=[embedding_matrix],
                                 input_length=padding_length)
     model.add(embedding_layer)
-    model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+    model.add(LSTM(lstm_dimensionality, dropout=dropout, recurrent_dropout=recurrent_dropout))
     model.add(Dense(number_of_classes))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy',
@@ -59,6 +62,7 @@ def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping
                   metrics=['accuracy'])
     return model
 
+
 def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
@@ -79,6 +83,7 @@ def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length):
                   optimizer='adam',
                   metrics=['accuracy'])
     return model
+
 
 def blstm(number_of_classes, index_to_embedding_mapping, padding_length):
     embedding_dimension = len(index_to_embedding_mapping[0])
