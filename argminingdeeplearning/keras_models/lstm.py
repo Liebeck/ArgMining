1a85127afb503b7139b670679df9cbd7ae7d9e4a
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-10-26 18:46:55 +0200

    Rename parameter

--- argminingdeeplearning/keras_models/lstm.py
+++ argminingdeeplearning/keras_models/lstm.py
@@ -16,7 +16,7 @@ def lstm_embedding_empty(number_of_classes, max_features=7000, lstm_size=128, dr
                   metrics=['accuracy'])
     return model
 
-def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, input_length):
+def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, padding_length):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
     print('keys.len: {}'.format(number_of_words))
@@ -27,7 +27,7 @@ def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, inp
     embedding_layer = Embedding(number_of_words,
                                 embedding_dimension,
                                 weights=[embedding_matrix],
-                                input_length=input_length)
+                                input_length=padding_length)
     model.add(embedding_layer)
     model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
     model.add(Dense(number_of_classes))
@@ -38,7 +38,7 @@ def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, inp
     return model
 
 
-def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping, input_length):
+def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping, padding_length):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
     print('keys.len: {}'.format(number_of_words))
@@ -49,7 +49,7 @@ def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping
     embedding_layer = Embedding(number_of_words,
                                 embedding_dimension,
                                 weights=[embedding_matrix],
-                                input_length=input_length)
+                                input_length=padding_length)
     model.add(embedding_layer)
     model.add(LSTM(128, dropout=0.5, recurrent_dropout=0.4))
     model.add(Dense(number_of_classes))
@@ -59,7 +59,7 @@ def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping
                   metrics=['accuracy'])
     return model
 
-def lst_stacked(number_of_classes, index_to_embedding_mapping, input_length):
+def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
     embedding_matrix = np.zeros((number_of_words, embedding_dimension))
@@ -69,7 +69,7 @@ def lst_stacked(number_of_classes, index_to_embedding_mapping, input_length):
     embedding_layer = Embedding(number_of_words,
                                 embedding_dimension,
                                 weights=[embedding_matrix],
-                                input_length=input_length)
+                                input_length=padding_length)
     model.add(embedding_layer)
     model.add(LSTM(128, dropout=0.5, recurrent_dropout=0.4, return_sequences=True))
     model.add(LSTM(64, dropout=0.5, recurrent_dropout=0.4))
@@ -80,7 +80,7 @@ def lst_stacked(number_of_classes, index_to_embedding_mapping, input_length):
                   metrics=['accuracy'])
     return model
 
-def blstm(number_of_classes, index_to_embedding_mapping, input_length):
+def blstm(number_of_classes, index_to_embedding_mapping, padding_length):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
     embedding_matrix = np.zeros((number_of_words, embedding_dimension))
@@ -90,7 +90,7 @@ def blstm(number_of_classes, index_to_embedding_mapping, input_length):
     embedding_layer = Embedding(number_of_words,
                                 embedding_dimension,
                                 weights=[embedding_matrix],
-                                input_length=input_length)
+                                input_length=padding_length)
     model.add(embedding_layer)
     # model.add(Bidirectional(LSTM(128, dropout=0.5, return_sequences=True)))
     # model.add(Bidirectional(LSTM(64, dropout=0.5)))
