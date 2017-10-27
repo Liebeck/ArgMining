db5879604bd744e3a1a1eb00e2ab5a0de3f6eef6
Liebeck <Liebeck@users.noreply.github.com>
2017-10-27 13:23:50 +0200

    Cleanup

--- argminingdeeplearning/keras_models/lstm.py
+++ argminingdeeplearning/keras_models/lstm.py
@@ -19,10 +19,9 @@ def lstm_embedding_empty(number_of_classes, max_features=7000, embedding_size=30
 
 
 def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, padding_length,
-                              lstm_dimensionality=128, dropout=0.2, recurrent_dropout=0.2):
+                              lstm_size=128, dropout=0.2, recurrent_dropout=0.2):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
-    print('keys.len: {}'.format(number_of_words))
     embedding_matrix = np.zeros((number_of_words, embedding_dimension))
     for index, embedding in index_to_embedding_mapping.items():
         embedding_matrix[index] = embedding
@@ -32,7 +31,7 @@ def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, pad
                                 weights=[embedding_matrix],
                                 input_length=padding_length)
     model.add(embedding_layer)
-    model.add(LSTM(lstm_dimensionality, dropout=dropout, recurrent_dropout=recurrent_dropout))
+    model.add(LSTM(lstm_size, dropout=dropout, recurrent_dropout=recurrent_dropout))
     model.add(Dense(number_of_classes))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy',
@@ -41,26 +40,26 @@ def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, pad
     return model
 
 
-def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping, padding_length):
-    embedding_dimension = len(index_to_embedding_mapping[0])
-    number_of_words = len(index_to_embedding_mapping.keys())
-    print('keys.len: {}'.format(number_of_words))
-    embedding_matrix = np.zeros((number_of_words, embedding_dimension))
-    for index, embedding in index_to_embedding_mapping.items():
-        embedding_matrix[index] = embedding
-    model = Sequential()
-    embedding_layer = Embedding(number_of_words,
-                                embedding_dimension,
-                                weights=[embedding_matrix],
-                                input_length=padding_length)
-    model.add(embedding_layer)
-    model.add(LSTM(128, dropout=0.5, recurrent_dropout=0.4))
-    model.add(Dense(number_of_classes))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    return model
+# def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping, padding_length,
+#                                    lstm_dimensionality=128, dropout=0.5, recurrent_dropout=0.4):
+#     embedding_dimension = len(index_to_embedding_mapping[0])
+#     number_of_words = len(index_to_embedding_mapping.keys())
+#     embedding_matrix = np.zeros((number_of_words, embedding_dimension))
+#     for index, embedding in index_to_embedding_mapping.items():
+#         embedding_matrix[index] = embedding
+#     model = Sequential()
+#     embedding_layer = Embedding(number_of_words,
+#                                 embedding_dimension,
+#                                 weights=[embedding_matrix],
+#                                 input_length=padding_length)
+#     model.add(embedding_layer)
+#     model.add(LSTM(lstm_dimensionality, dropout=dropout, recurrent_dropout=recurrent_dropout))
+#     model.add(Dense(number_of_classes))
+#     model.add(Activation('softmax'))
+#     model.compile(loss='categorical_crossentropy',
+#                   optimizer='adam',
+#                   metrics=['accuracy'])
+#     return model
 
 
 def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length):
