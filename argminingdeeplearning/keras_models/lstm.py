76c6cffce7a1ca093f9f7b5afe5035daba40680f
Liebeck <Liebeck@users.noreply.github.com>
2017-10-27 14:51:24 +0200

    Refactoring

--- argminingdeeplearning/keras_models/lstm.py
+++ argminingdeeplearning/keras_models/lstm.py
@@ -39,29 +39,8 @@ def lstm_embedding_pretrained(number_of_classes, index_to_embedding_mapping, pad
     return model
 
 
-# def lstm_embedding_pretrained_test(number_of_classes, index_to_embedding_mapping, padding_length,
-#                                    lstm_dimensionality=128, dropout=0.5, recurrent_dropout=0.4):
-#     embedding_dimension = len(index_to_embedding_mapping[0])
-#     number_of_words = len(index_to_embedding_mapping.keys())
-#     embedding_matrix = np.zeros((number_of_words, embedding_dimension))
-#     for index, embedding in index_to_embedding_mapping.items():
-#         embedding_matrix[index] = embedding
-#     model = Sequential()
-#     embedding_layer = Embedding(number_of_words,
-#                                 embedding_dimension,
-#                                 weights=[embedding_matrix],
-#                                 input_length=padding_length)
-#     model.add(embedding_layer)
-#     model.add(LSTM(lstm_dimensionality, dropout=dropout, recurrent_dropout=recurrent_dropout))
-#     model.add(Dense(number_of_classes))
-#     model.add(Activation('softmax'))
-#     model.compile(loss='categorical_crossentropy',
-#                   optimizer='adam',
-#                   metrics=['accuracy'])
-#     return model
-
-
-def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length):
+def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length, lstm_size_layer1=128,
+                lstm_size_layer2=64, dropout=0.5, recurrent_dropout=0.4):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
     embedding_matrix = np.zeros((number_of_words, embedding_dimension))
@@ -73,8 +52,8 @@ def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length):
                                 weights=[embedding_matrix],
                                 input_length=padding_length)
     model.add(embedding_layer)
-    model.add(LSTM(128, dropout=0.5, recurrent_dropout=0.4, return_sequences=True))
-    model.add(LSTM(64, dropout=0.5, recurrent_dropout=0.4))
+    model.add(LSTM(lstm_size_layer1, dropout=dropout, recurrent_dropout=recurrent_dropout, return_sequences=True))
+    model.add(LSTM(lstm_size_layer2, dropout=dropout, recurrent_dropout=recurrent_dropout))
     model.add(Dense(number_of_classes))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy',
@@ -83,7 +62,8 @@ def lst_stacked(number_of_classes, index_to_embedding_mapping, padding_length):
     return model
 
 
-def blstm(number_of_classes, index_to_embedding_mapping, padding_length):
+def blstm(number_of_classes, index_to_embedding_mapping, padding_length, lstm_size_layer1=128,
+          lstm_size_layer2=64, dropout=0.5):
     embedding_dimension = len(index_to_embedding_mapping[0])
     number_of_words = len(index_to_embedding_mapping.keys())
     embedding_matrix = np.zeros((number_of_words, embedding_dimension))
@@ -98,8 +78,8 @@ def blstm(number_of_classes, index_to_embedding_mapping, padding_length):
     # model.add(Bidirectional(LSTM(128, dropout=0.5, return_sequences=True)))
     # model.add(Bidirectional(LSTM(64, dropout=0.5)))
     # model.add(Bidirectional(LSTM(128, dropout=0.8, return_sequences=True)))
-    model.add(Bidirectional(LSTM(64, dropout=0.85, return_sequences=True)))
-    model.add(Bidirectional(LSTM(32, dropout=0.85)))
+    model.add(Bidirectional(LSTM(lstm_size_layer1, dropout=dropout, return_sequences=True)))
+    model.add(Bidirectional(LSTM(lstm_size_layer2, dropout=dropout)))
     model.add(Dense(number_of_classes))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy',
